frida/frida-objc-bridge

commit 6edb9aef88b691be7c4cd44ca25aa27bd8f2a1f5

Fix signature frame size and offsets computation

index.js
@@ -2090,10 +2090,17 @@ function Runtime() {
     }
 
     function unparseSignature(retType, argTypes) {
-        const frameSize = argTypes.length * pointerSize;
-        return typeIdFromAlias(retType) + frameSize + argTypes.map(function (argType, i) {
-            const frameOffset = (i * pointerSize);
-            return typeIdFromAlias(argType) + frameOffset;
+        const retTypeId = typeIdFromAlias(retType);
+        const argTypeIds = argTypes.map(typeIdFromAlias);
+
+        const argSizes = argTypeIds.map(id => singularTypeById[id].size);
+        const frameSize = argSizes.reduce((total, size) => total + size, 0);
+
+        let frameOffset = 0;
+        return retTypeId + frameSize + argTypeIds.map((id, i) => {
+            const result = id + frameOffset;
+            frameOffset += argSizes[i];
+            return result;
         }).join("");
     }
 

commit 6cb572c489419d8dbe431a99a88d7ccee959c1e8

Fix “pointer” support in method and block definitions
This regression was introduced in 6edb9ae.

index.js
@@ -2627,6 +2627,12 @@ function Runtime() {
             write: (address, value) => { address.writePointer(value); },
             toNative: toNativeObjectArray
         },
+        '^v': {
+            type: 'pointer',
+            size: pointerSize,
+            read: address => address.readPointer(),
+            write: (address, value) => { address.writePointer(value); },
+        },
         '#': {
             type: 'pointer',
             size: pointerSize,

test/basics.m
@@ -63,6 +63,7 @@ TESTLIST_END ()
 @protocol FridaCalculator
 - (int)add:(int)value;
 - (void)add:(int)value completion:(void (^)(int, NSError *))block;
+- (void)addSquared:(int)value completion:(void (^)(int, void *))block;
 - (int)sub:(int)value;
 @optional
 - (int)magic;
@@ -85,7 +86,12 @@ TESTLIST_END ()
 - (int)add:(int)value { return 1337 + value; }
 - (void)add:(int)value completion:(void (^)(int, NSError *))block {
   dispatch_async (dispatch_get_global_queue (DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-    block(1337 + value, nil);
+    block (1337 + value, nil);
+  });
+}
+- (void)addSquared:(int)value completion:(void (^)(int, void *))block {
+  dispatch_async (dispatch_get_global_queue (DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+    block (1337 + (value * value), GSIZE_TO_POINTER (0x42));
   });
 }
 - (int)sub:(int)value { return 1337 - value; }
@@ -438,6 +444,20 @@ TESTCASE (block_can_be_implemented)
       "calc.add_completion_(3, onComplete);",
       calc);
   EXPECT_SEND_MESSAGE_WITH ("[1340,null]");
+  EXPECT_NO_MESSAGES ();
+
+  COMPILE_AND_LOAD_SCRIPT (
+      "var calc = new ObjC.Object(" GUM_PTR_CONST ");"
+      "var onComplete = new ObjC.Block({"
+          "retType: 'void',"
+          "argTypes: ['int', 'pointer'],"
+          "implementation: function (result, data) {"
+              "send([result, data]);"
+          "}"
+      "});"
+      "calc.addSquared_completion_(7, onComplete);",
+      calc);
+  EXPECT_SEND_MESSAGE_WITH ("[1386,\"0x42\"]");
   EXPECT_NO_MESSAGES ();
 }
 